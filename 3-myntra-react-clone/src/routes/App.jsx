bc0fe12e858a9ca3869c09d1224a7e89dd21bb40
Manik31Raj <108018610+Manik31Raj@users.noreply.github.com>
2025-05-31 13:54:02 +0530

    Add files via upload

---
+++ 3-myntra-react-clone/src/routes/App.jsx
@@ -0,0 +1,17 @@
+import Header from "../components/Header.jsx";
+import Footer from "../components/Footer.jsx";
+import {Outlet} from "react-router-dom";
+import FetchItems from "../components/FetchItems.jsx";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <FetchItems />
+      <Outlet />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
